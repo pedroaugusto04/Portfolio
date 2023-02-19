e7d543afb61c24ecee283bf4396a1ae657543928
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2023-02-19 20:31:47 -0300

    load adjustment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-window.onload = function() {
+
+setTimeout(load,1000);
+// show loading page
+
+function load(){
+  window.onload = loadLogic();
+}
+function loadLogic(){ 
   var div = document.getElementById("load");
   div.remove();
   document.getElementById("content").style.display = "block";
